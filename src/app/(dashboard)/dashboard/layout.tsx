a4f1070fffe99d88d43f044b0784d5da4b17dada
AminGhasempoor <aminghasempoor79@gmail.com>
2025-11-04 14:16:36 +0330

    user dashboard

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,19 +1,28 @@
-import DashboardList from "@/app/(dashboard)/dashboard/_component/DashboardList";
+import DashboardList from "@/components/Dashboard/DashboardList";
+import CreateWorkSpace from "@/components/Dashboard/CreateWorkSpace";
+import UserNav from "@/components/Dashboard/UserNav";
+import {ReactNode} from "react";
 
 export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                            children,
+                                        }: {
+    children: ReactNode;
 }) {
-  return (
-    <div className={"flex w-full h-screen"}>
-      <div
-        className={
-          "flex h-full flex-col items-center bg-secondary py-3 px-2 border-r border-border"
-        }
-      >
-        <DashboardList />
-      </div>
-    </div>
-  );
+    return (
+        <div className={"flex w-full h-screen"}>
+            <div
+                className={"flex h-full w-16 flex-col items-center justify-between bg-secondary py-3 px-2 border-r border-border"}>
+                <div>
+                    <DashboardList/>
+                    <div className={"mt-4"}>
+                        <CreateWorkSpace/>
+                    </div>
+                </div>
+                <div className={"mt-auto"}>
+                    <UserNav/>
+                </div>
+            </div>
+            {children}
+        </div>
+    );
 }
